f5109224f7bf5aa3426b0d1c513d057fefaff076
Alain Ranger <alainranger@gmail.com>
2025-05-08 17:52:24 -0400

    initital commit

---
+++ AspireAppDemo.AppHost/Program.cs
@@ -0,0 +1,21 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+// Database 
+var postgres = builder.AddPostgres("postgres");
+var postgresdb = postgres.AddDatabase("postgresdb");
+
+// Backend
+var apiService = builder.AddProject<Projects.AspireAppDemo_ApiService>("apiservice")
+    .WithHttpsHealthCheck("/health")
+    .WithReference(postgresdb)
+    .WaitFor(postgresdb);
+
+// Frontend
+// Blazor
+builder.AddProject<Projects.AspireAppDemo_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithHttpsHealthCheck("/health")
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.Build().Run();
